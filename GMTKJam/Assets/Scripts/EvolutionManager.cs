00101bfac1b22ebab87379a4b6d9cbc3098980df
atillagallio <atilla.gallio@gmail.com>
2018-09-01 17:34:02 -0300

    Evolution Verifier

---
+++ GMTKJam/Assets/Scripts/EvolutionManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvolutionManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    public CharacterStats charInfo;
+
+    public int adultAge;
+    public int seniorAge;
+
+    public float evolutionThreshold;
+
+    public enum EvolutionType
+    {
+        AdultNormal,
+        Entrepreneur,
+        Hippie,
+        Punk,
+        OldNormal,
+        OldAtlete,
+        OldWheelchair,
+        OldZen,
+    }
+
+    public EvolutionType VerifyEvolution()
+    {
+
+        int entrepreneurInfluence = 0;
+        int hippieInfluence = 0;
+        int punkInfluence = 0;
+        int atleteInfluence = 0;
+        int wheelchairInfluence = 0;
+        int zenInfluence = 0;
+
+
+        Dictionary<EvolutionType, Vector2> evolutionVector = new Dictionary<EvolutionType, Vector2>();
+        evolutionVector.Add(EvolutionType.Entrepreneur, Vector2.right);
+        evolutionVector.Add(EvolutionType.Hippie, Vector2.right.Rotate(120f));
+        evolutionVector.Add(EvolutionType.Punk, Vector2.right.Rotate(240f));
+        evolutionVector.Add(EvolutionType.OldAtlete, Vector2.right);
+        evolutionVector.Add(EvolutionType.OldWheelchair, Vector2.right.Rotate(120f));
+        evolutionVector.Add(EvolutionType.OldZen, Vector2.right.Rotate(240f));
+
+
+        foreach (var item in charInfo.ItemCount)
+        {
+            entrepreneurInfluence += item.Key.entrepreneurInfluence * item.Value;
+            hippieInfluence += item.Key.hippieInfluence * item.Value;
+            punkInfluence += item.Key.punkInfluence * item.Value;
+            atleteInfluence += item.Key.atleteInfluence * item.Value;
+            wheelchairInfluence += item.Key.wheelchairInfluence * item.Value;
+            zenInfluence += item.Key.zenInfluence * item.Value;
+        }
+
+        evolutionVector[EvolutionType.Entrepreneur] *= entrepreneurInfluence;
+        evolutionVector[EvolutionType.Hippie] *= hippieInfluence;
+        evolutionVector[EvolutionType.Punk] *= punkInfluence;
+        evolutionVector[EvolutionType.OldAtlete] *= atleteInfluence;
+        evolutionVector[EvolutionType.OldWheelchair] *= wheelchairInfluence;
+        evolutionVector[EvolutionType.OldZen] *= zenInfluence;
+
+        Vector2 resultVector = Vector2.zero;
+        Dictionary<float, EvolutionType> EvolutionDots = new Dictionary<float, EvolutionType>();
+        if (charInfo.Age >= adultAge && charInfo.Age <= seniorAge)
+        {
+            resultVector = evolutionVector[EvolutionType.Entrepreneur] + evolutionVector[EvolutionType.Hippie] + evolutionVector[EvolutionType.Punk];
+            EvolutionDots.Add(Vector2.Dot(resultVector, evolutionVector[EvolutionType.Entrepreneur]), EvolutionType.Entrepreneur);
+            EvolutionDots.Add(Vector2.Dot(resultVector, evolutionVector[EvolutionType.Hippie]), EvolutionType.Hippie);
+            EvolutionDots.Add(Vector2.Dot(resultVector, evolutionVector[EvolutionType.Punk]), EvolutionType.Punk);
+        }
+        else if (charInfo.Age >= seniorAge)
+        {
+            resultVector = evolutionVector[EvolutionType.OldAtlete] + evolutionVector[EvolutionType.OldWheelchair] + evolutionVector[EvolutionType.OldZen];
+            EvolutionDots.Add(Vector2.Dot(resultVector, evolutionVector[EvolutionType.OldAtlete]), EvolutionType.OldAtlete);
+            EvolutionDots.Add(Vector2.Dot(resultVector, evolutionVector[EvolutionType.OldWheelchair]), EvolutionType.OldWheelchair);
+            EvolutionDots.Add(Vector2.Dot(resultVector, evolutionVector[EvolutionType.OldZen]), EvolutionType.OldZen);
+        }
+
+
+        if (resultVector.magnitude <= evolutionThreshold)
+            return EvolutionType.AdultNormal;
+
+
+        float biggestDotValue = 0;
+        foreach (var value in EvolutionDots.Keys)
+        {
+            if (value >= biggestDotValue) biggestDotValue = value;
+        }
+
+        print(EvolutionDots[biggestDotValue]);
+        return EvolutionDots[biggestDotValue];
+    }
+}
--- GMTKJam/Assets/Scripts/Item.cs
+++ GMTKJam/Assets/Scripts/Item.cs
@@ -6,72 +6,82 @@ using System;
 [Serializable]
 public struct Condition
 {
-  public Item preReqItem;
-  public int NumberOfTimes;
+    public Item preReqItem;
+    public int NumberOfTimes;
 
-  public ConditionLogic conditionLogic;
+    public ConditionLogic conditionLogic;
 
-  public delegate bool Comparison(int a, int b);
+    public delegate bool Comparison(int a, int b);
 
-  public static Dictionary<ConditionLogic, Comparison> conditionMap = new Dictionary<ConditionLogic, Comparison>(){
+    public static Dictionary<ConditionLogic, Comparison> conditionMap = new Dictionary<ConditionLogic, Comparison>(){
         {ConditionLogic.Greater, (a,b)=>a>b},
         {ConditionLogic.Equal, (a,b)=>a==b},
         {ConditionLogic.Less, (a,b)=>a<b},
     };
-  public bool VerifyCondition(int itensCount)
-  {
-    return conditionMap[conditionLogic](itensCount, NumberOfTimes);
-  }
+    public bool VerifyCondition(int itensCount)
+    {
+        return conditionMap[conditionLogic](itensCount, NumberOfTimes);
+    }
 
 }
 
 public enum ConditionLogic
 {
-  Greater,
-  Equal,
-  Less,
+    Greater,
+    Equal,
+    Less,
 }
 
 public enum ConditionType
 {
-  Or,
-  And,
+    Or,
+    And,
 }
 
 public enum ItemType
 {
-  Unique,
-  Addictive,
-  Repeatable,
+    Unique,
+    Addictive,
+    Repeatable,
 }
 
 public enum AdultInfluence
 {
-  Normal,
-  Empresario,
-  Hippie,
-  Punk,
+    Normal,
+    Empresario,
+    Hippie,
+    Punk,
 }
 
 public enum OldInfluence
 {
-  Normal,
-  Atleta,
-  Cadeirante,
-  Zen,
+    Normal,
+    Atleta,
+    Cadeirante,
+    Zen,
 }
 
 [CreateAssetMenu(fileName = "new Item", menuName = "ScriptableObj/Item")]
 public class Item : ScriptableObject
 {
-  public GameObject visual;
-  public ItemType itemType;
-  public int id;
-  public string itemName;
-  public float minAge;
-  public float maxAge;
-  public AdultInfluence adultInfluence;
-  public OldInfluence oldInfluence;
-  public ConditionType conditionType;
-  public List<Condition> conditionList;
+    public GameObject visual;
+    public ItemType itemType;
+    public int id;
+    public string itemName;
+    public float minAge;
+    public float maxAge;
+    public AdultInfluence adultInfluence;
+    public OldInfluence oldInfluence;
+    public ConditionType conditionType;
+    public List<Condition> conditionList;
+
+    [Header("Adult Influences")]
+    public int entrepreneurInfluence;
+    public int hippieInfluence;
+    public int punkInfluence;
+
+    [Header("Old Influences")]
+    public int atleteInfluence;
+    public int wheelchairInfluence;
+    public int zenInfluence;
 }
