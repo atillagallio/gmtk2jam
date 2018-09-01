4cb38af8d813b073819f0f1f330c27b58c5c32ce
mut <gustavo.ceci95@gmail.com>
2018-09-01 20:24:26 -0300

    mais coisas quebradas de animacao e paralaxe linda

--- GMTKJam/Assets/ParallaxController.cs
+++ GMTKJam/Assets/ParallaxController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -17,6 +18,15 @@ public class ParallaxController : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-
+    Action<ParallaxThing> move = el =>
+    {
+      if (transform.position.x - el.transform.position.x > el.Side)
+      {
+        el.transform.position += 2.0f * Vector3.right * el.Side;
+      }
+    };
+    LayerCopy.ForEach(move);
+    LayerDefinition.ForEach(move);
   }
 }
+
