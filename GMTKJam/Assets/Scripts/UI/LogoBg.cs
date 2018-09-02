bc041474d6abaa11aa078b7012ce26ce02ee10ae
atillagallio <atilla.gallio@gmail.com>
2018-09-02 04:43:26 -0300

    logo done

---
+++ GMTKJam/Assets/Scripts/UI/LogoBg.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogoBg : MonoBehaviour
+{
+
+    public float speedRightAxis;
+    public float speedUpAxis;
+    public float speedForwardAxis;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right * Time.deltaTime * speedRightAxis);
+        transform.Rotate(Vector3.up * Time.deltaTime * speedUpAxis);
+        transform.Rotate(Vector3.forward * Time.deltaTime * speedForwardAxis);
+
+    }
+}
